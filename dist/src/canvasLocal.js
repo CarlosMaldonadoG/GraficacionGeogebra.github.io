e5356bc3fd923f2c2600f85fa920fc22183e1a98
CarlosMaldonadoG <buddylxp97@gmail.com>
2022-04-09 11:53:11 -0500

    dibujamos cuadricula y la funcion de x

--- dist/src/canvasLocal.js
+++ dist/src/canvasLocal.js
@@ -1,14 +1,16 @@
 export class CanvasLocal {
     constructor(g, canvas) {
         this.graphics = g;
-        this.rWidth = 6;
-        this.rHeight = 4;
+        this.rWidth = 24.0;
+        this.rHeight = 20.0;
         this.maxX = canvas.width - 1;
         this.maxY = canvas.height - 1;
         this.pixelSize = Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
         this.centerX = this.maxX / 2;
         this.centerY = this.maxY / 2;
     }
+    iX(x) { return Math.round(this.centerX + x / this.pixelSize); }
+    iY(y) { return Math.round(this.centerY - y / this.pixelSize); }
     drawLine(x1, y1, x2, y2) {
         this.graphics.beginPath();
         this.graphics.moveTo(x1, y1);
@@ -16,9 +18,20 @@ export class CanvasLocal {
         this.graphics.closePath();
         this.graphics.stroke();
     }
+    fx(x) {
+        return x * x;
+    }
     paint() {
-        this.drawLine(100.5, 100, 500, 100.5);
-        this.drawLine(500, 100, 300, 400);
-        this.drawLine(300, 400, 100, 100);
+        this.graphics.strokeStyle = 'black';
+        for (let i = -10; i <= 10; i++) {
+            this.drawLine(this.iX(i), this.iY(-10), this.iX(i), this.iY(10));
+            this.graphics.fillText(i + "", this.iX(i), this.iY(-0.3));
+            this.drawLine(this.iX(-10), this.iY(i), this.iX(10), this.iY(i));
+            this.graphics.fillText(i + "", this.iX(-0.2), this.iY(i));
+        }
+        this.graphics.strokeStyle = 'red';
+        for (let x = -3; x <= 3; x += 0.1) {
+            this.drawLine(this.iX(x), this.iY(this.fx(x)), this.iX(x + 0.1), this.iY(this.fx(x + 0.1)));
+        }
     }
 }
