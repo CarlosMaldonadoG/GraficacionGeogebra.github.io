9f7ea97a2da5c6ec1e22e2cb785ec26545cc8665
CarlosMaldonadoG <buddylxp97@gmail.com>
2022-04-22 15:00:37 -0500

    Copiando Barras

--- src/index.ts
+++ src/index.ts
@@ -6,44 +6,6 @@ let graphics: CanvasRenderingContext2D;
 canvas = <HTMLCanvasElement>document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 
-const inputUno = <HTMLInputElement>document.querySelector('.field-number');
-const inputDos = <HTMLInputElement>document.querySelector('.number-filed');
+const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
 
-function obtenerValores(){
-    limpiarCanvas();
-    let valueUno = parseInt(inputUno.value);
-    let valueDos = parseInt(inputDos.value);
-    if(inputUno.value === '' || inputDos.value === ''){
-        valueUno = 24;
-        valueDos = 22;
-    }else{
-        valueUno = valueUno;
-        valueDos = valueDos;
-    }
-    if(valueUno < 0 || valueDos < 0){
-        alert("Numeros negativos no  son aceptados");
-        dibujoPorDefecto();
-        inputUno.value = "";
-        inputDos.value = "";
-    }else{
-        inputUno.value = "";
-        inputDos.value = "";
-        const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas, valueUno, valueDos);
-        miCanvas.paint();
-    }
-}
-
-function limpiarCanvas(){
-    graphics.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function dibujoPorDefecto(){
-    let valorUnoInicial = 20;
-    let valorDosInicial = 18;
-    const miCanvas : CanvasLocal = new CanvasLocal(graphics, canvas, valorUnoInicial, valorDosInicial);
-    miCanvas.paint(); 
-}
-
-dibujoPorDefecto();
-
-document.getElementById('btn-enviar').addEventListener('click', obtenerValores, false);
+miCanvas.paint();
