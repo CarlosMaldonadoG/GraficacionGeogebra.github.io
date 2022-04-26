d96ba13cb4931494a1453b564069fa712051b0fb
CarlosMaldonadoG <buddylxp97@gmail.com>
2022-04-26 13:17:54 -0500

    Transcripcion final

--- dist/src/canvasLocal.js
+++ dist/src/canvasLocal.js
@@ -1,14 +1,19 @@
+import { Play } from "./play.js";
 export class CanvasLocal {
-    constructor(g, canvas, arrayTam) {
+    constructor(g, canvas) {
         this.graphics = g;
-        this.rWidth = (arrayTam * 2) + 5;
-        this.rHeight = 14;
+        this.mallaSize = 50;
+        this.rWidth = this.mallaSize * 1.33;
+        this.rHeight = this.mallaSize;
         this.maxX = canvas.width - 1;
         this.maxY = canvas.height - 1;
         this.pixelSize = Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
-        this.centerX = this.maxX / this.rWidth;
-        this.centerY = this.maxY / 8 * 6;
-        console.log(this.centerY);
+        this.centerX = this.maxX / 12 * 1.5;
+        this.centerY = this.maxY - 2;
+        this.step = 0;
+        this.blockSize = this.iX(1) - this.iX(0);
+        this.juego = new Play(this.mallaSize);
+        this.juego.generaMatriz2();
     }
     iX(x) { return Math.round(this.centerX + x / this.pixelSize); }
     iY(y) { return Math.round(this.centerY - y / this.pixelSize); }
@@ -19,83 +24,26 @@ export class CanvasLocal {
         this.graphics.closePath();
         this.graphics.stroke();
     }
-    drawRmboide(x1, y1, x2, y2, x3, y3, x4, y4, color) {
-        this.graphics.fillStyle = color;
-        this.graphics.beginPath();
-        this.graphics.moveTo(x1, y1);
-        this.graphics.lineTo(x2, y2);
-        this.graphics.lineTo(x3, y3);
-        this.graphics.lineTo(x4, y4);
-        this.graphics.closePath();
-        this.graphics.stroke();
-        this.graphics.fill();
+    fx(x) {
+        return Math.sin(x * 2.5);
     }
-    maxH(h) {
-        let max = h[0];
-        for (let i = 1; i < h.length; i++) {
-            if (max < h[i])
-                max = h[i];
-        }
+    getGraphics() {
+        return this.graphics;
+    }
+    paint() {
         let res;
-        let pot = 10;
-        while (pot < max) {
-            pot *= 10;
-        }
-        pot /= 10;
-        res = Math.ceil(max / pot) * pot;
-        return res;
-    }
-    paint(valores) {
-        let h = valores;
-        let maxEsc;
-        let xNum;
-        let colors = ['magenta', 'red', 'green', 'yellow'];
-        maxEsc = this.maxH(h);
-        xNum = h.length * 2;
-        this.graphics.strokeStyle = 'black';
-        this.drawLine(this.iX(0), this.iY(0), this.iX(xNum), this.iY(0));
-        this.drawLine(this.iX(0), this.iY(0), this.iX(0), this.iY(6));
-        let i = 0;
-        for (let y = 0.6; y <= 6; y += 1.35) {
-            this.drawLine(this.iX(0.6), this.iY(y), this.iX(xNum), this.iY(y));
-            this.drawLine(this.iX(0), this.iY(y - 0.6), this.iX(0.6), this.iY(y));
-            this.graphics.strokeText((maxEsc * i / 4) + "", this.iX(-0.5), this.iY(y - 0.7));
-            i++;
-        }
-        this.graphics.strokeStyle = 'black';
-        let ind = 0;
-        let cont = 0;
-        for (let i = 0.5; i <= xNum; i += 2) {
-            this.graphics.fillStyle = colors[cont];
-            this.drawLine(this.iX(i), this.iY(6 * h[ind] / maxEsc - 0.1), this.iX(i), this.iY(0.1));
-            this.graphics.fillRect(this.iX(i), this.iY(6 * h[ind] / maxEsc - 0.1), this.iX(2) - this.iX(1), this.iY(0.2) - this.iY(6 * h[ind] / maxEsc));
-            this.drawRmboide(this.iX(i + 0.3), this.iY(6 * h[ind] / maxEsc + 0.2), this.iX(i + 1.3), this.iY(6 * h[ind] / maxEsc + 0.2), this.iX(i + 1), this.iY(6 * h[ind] / maxEsc - 0.1), this.iX(i), this.iY(6 * h[ind] / maxEsc - 0.1), colors[cont]);
-            this.drawRmboide(this.iX(i + 1), this.iY(6 * h[ind] / maxEsc - 0.1), this.iX(i + 1.3), this.iY(6 * h[ind] / maxEsc + 0.2), this.iX(i + 1.3), this.iY(0.4), this.iX(i + 1), this.iY(0.1), colors[cont]);
-            ind++;
-            cont++;
-            if (cont === 4) {
-                cont = 0;
+        this.graphics.fillStyle = 'white';
+        this.graphics.fillRect(0, 0, this.maxX, this.maxY);
+        this.graphics.strokeStyle = 'lightgray';
+        res = this.juego.getMat();
+        this.graphics.fillStyle = 'black';
+        for (let i = 0; i < this.mallaSize; i++) {
+            for (let j = 0; j < this.mallaSize; j++) {
+                if (res[i][j] == 1)
+                    this.graphics.fillRect(this.iX(i), this.iY(j), this.blockSize, this.blockSize);
             }
         }
-        cont = 0;
-        for (let x = 0; x < xNum; x += 2) {
-            this.graphics.strokeText(colors[cont++], this.iX(x + 0.5), this.iY(-0.5));
-            if (cont === 4) {
-                cont = 0;
-            }
-        }
-        cont = 0;
-        ind = 0;
-        for (let y = 0; y < h.length; y++) {
-            this.graphics.strokeText(colors[cont], this.iX(xNum + 1), this.iY(8 - y));
-            this.graphics.strokeText(" " + h[ind], this.iX(xNum + 2.5), this.iY(8 - y));
-            this.graphics.fillStyle = colors[cont];
-            this.graphics.fillRect(this.iX(xNum + 0.5), this.iY(8 - y), 10, 10);
-            cont++;
-            ind++;
-            if (cont === 4) {
-                cont = 0;
-            }
-        }
+        res = this.juego.juegoDeLaVida(this.juego.getMat());
+        this.juego.setMat(res);
     }
 }
--- dist/src/index.js
+++ dist/src/index.js
@@ -3,23 +3,8 @@ let canvas;
 let graphics;
 canvas = document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
-const inputUno = document.querySelector('.field-text');
-function obtenerValores() {
-    limpiarCanvas();
-    let tamArray;
-    let valueUno = inputUno.value;
-    let valores = valueUno.split(',').map(elem => parseFloat(elem));
-    tamArray = valores.length;
-    if (inputUno.value === "") {
-        alert("Ingrese los valores para poder graficar");
-    }
-    else {
-        const miCanvas = new CanvasLocal(graphics, canvas, tamArray);
-        miCanvas.paint(valores);
-        inputUno.value = "";
-    }
+const miCanvas = new CanvasLocal(graphics, canvas);
+function animate() {
+    miCanvas.paint();
 }
-function limpiarCanvas() {
-    graphics.clearRect(0, 0, canvas.width, canvas.height);
-}
-document.getElementById('btn-valor').addEventListener('click', obtenerValores, false);
+setInterval(animate, 100);
