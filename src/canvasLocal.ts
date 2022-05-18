82ee36e18403440421418b11364cb2b868982066
CarlosMaldonadoG <buddylxp97@gmail.com>
2022-05-17 20:02:58 -0500

    Adicionamos codigo funcionamiento CUBO3D

--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -1,5 +1,3 @@
-import { Play } from "./play.js";
-
 export class CanvasLocal {
   //atributos
   protected graphics: CanvasRenderingContext2D;
@@ -10,64 +8,26 @@ export class CanvasLocal {
   protected pixelSize: number;
   protected centerX: number;
   protected centerY: number;
-  step: number;
-  mallaSize: number;
-  juego: Play;
-  blockSize: number;
   
+      
   public constructor(g: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
     this.graphics = g;
-    this.mallaSize = 50;
-    this.rWidth = this.mallaSize*1.33;
-    this.rHeight= this.mallaSize;
+    this.rWidth = 10;
+    this.rHeight= 10;
     this.maxX = canvas.width - 1
     this.maxY = canvas.height - 1;
-    this.pixelSize = Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
-    this.centerX = this.maxX/12*1.5;
-    this.centerY = this.maxY-2;
-    this.step = 0;
-    
-    this.blockSize = this.iX(1) - this.iX(0);
-    //this.blockSize = 30;
-    this.juego = new Play(this.mallaSize);
-		this.juego.generaMatriz();
+    this.pixelSize = Math.min(this.maxX, this.maxY);//Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
+    this.centerX = this.maxX / 2;
+    this.centerY = this.maxY / 2;
   }
 
-  iX(x: number):number{return Math.round(this.centerX + x/this.pixelSize);}
-  iY(y: number): number{ return Math.round(this.centerY - y / this.pixelSize); }
-  
-  drawLine(x1: number, y1: number, x2: number, y2:number) {
-    this.graphics.beginPath();
-    this.graphics.moveTo(x1, y1);
-    this.graphics.lineTo(x2, y2);
-    this.graphics.closePath();
-    this.graphics.stroke();
-  }
-  
-  getGraphics(): CanvasRenderingContext2D{
-    return this.graphics;
-  }
+  iX( x: number):number{return Math.round(this.centerX + x);}
+  iY( y: number):number{return Math.round(this.centerY - y);}
 
   paint() {
-    let res: number[][];
-    
-    //dibuja la cuadricula
-    this.graphics.fillStyle = 'white';
-    this.graphics.fillRect(0, 0, this.maxX, this.maxY);
-    this.graphics.strokeStyle = 'lightgray'; 
-
-    //trazar el juego (los cuadritos) a partir de la matriz creada
-    res = this.juego.getMat();
-    this.graphics.fillStyle = 'black';
-		for(let i=0; i<this.mallaSize; i++){
-			for(let j=0;j<this.mallaSize; j++){	 
-        if (res[i][j] == 1)
-            this.graphics.fillRect(this.iX(i), this.iY(j), this.blockSize, this.blockSize);
-      }
-    }
-    //actualizar la matriz para la siguiente pasada
-		res=this.juego.juegoDeLaVida(this.juego.getMat());
-		this.juego.setMat(res);
+    this.graphics.arc(this.iX(0), this.iY(0), Math.abs(this.iX(4)-this.iX(0)), 0,2*Math.PI, false);
+    this.graphics.stroke();
+    this.graphics.fillText("Lienzo listo desde ts", this.iX(2), this.iY(3.7));
   }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { CanvasLocal } from './canvasLocal.js';
+import { CvCubePersp } from './cvCubePersp.js';
 
 let canvas: HTMLCanvasElement;
 let graphics: CanvasRenderingContext2D;
@@ -6,23 +6,6 @@ let graphics: CanvasRenderingContext2D;
 canvas = <HTMLCanvasElement>document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 
-const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
-
-//miCanvas.paint();
-let inteRval;
-let reproducir = false;
-
-function animate() {
-    miCanvas.paint();
-}
-
-function iniciar(){
-    inteRval = setInterval(animate, 90);
-}
-
-function pausa(){
-    clearInterval(inteRval);
-}
-
-document.getElementById('Reproducir').addEventListener('click', iniciar, false);
-document.getElementById('Pausa').addEventListener('click', pausa, false);
+//const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
+const miCanvas = new CvCubePersp (graphics, canvas);
+miCanvas.paint();
